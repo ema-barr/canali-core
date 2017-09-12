92ae83cd7f4dda9c13ef6c1ee0a67f3505fbda18
ema-barr <emanuele.barracchia@hotmail.it>
2017-09-12 18:34:50 +0200

    Aggiunte altre query

--- src/main/java/edu/ucla/cs/scai/canali/core/experiment/qald6/ExperimenterTest.java
+++ src/main/java/edu/ucla/cs/scai/canali/core/experiment/qald6/ExperimenterTest.java
@@ -11,9 +11,9 @@ public class ExperimenterTest {
 
 	public static void main(String[] args) throws Exception {
 
-		QASystem qas = new CanaliW2VQASystemD2RQ("/Users/Manu/Desktop/Semeraro/w2v/abstract_200_20.w2v.bin", "/Users/Manu/Desktop/Semeraro/kb-processed/supportFiles/property_labels");
+		QASystem qas = new CanaliW2VQASystemD2RQ("/Users/Manu/Desktop/Semeraro/w2v/abstract_200_20.w2v.bin", "/Users/Manu/Desktop/Semeraro/dvd processed/supportFiles/property_labels");
 		//System.setProperty("kb.index.dir", "/home/lucia/nlp2sparql-data/dbpedia-processed/2015-10/dbpedia-processed_onlydbo_mini_e/index/"); //!!!
-		System.setProperty("kb.index.dir", "/Users/Manu/Desktop/Semeraro/kb-processed/index");
+		System.setProperty("kb.index.dir", "/Users/Manu/Desktop/Semeraro/dvd processed/index");
 
 		System.setProperty("sparql.endpoint", "http://localhost:2020/sparql");
 
@@ -26,7 +26,8 @@ public class ExperimenterTest {
 		//String query = "What is the count of movies directed by Park Chan-wook?";
 		//String query = "What is the prize of Alain Connes?";
 		//String query = "Is there a award of Aki Kaurismäki equal to Grand Prix (Cannes Film Festival) ?";
-		
+		//------------------------------------------
+        //dvd
 		//String query = "What is the payment rental id of payment #17503?";
 		//String query = "What is the city #127?";
 		//String query = "What is the city country id of city #127?";
@@ -34,10 +35,14 @@ public class ExperimenterTest {
 		//String query = "What is the actor first_name of actor #27?";
 
 		//String query = "What is the count of city #127?";
-		//String query = "What is the count of film actor actor_id [inverted] of actor #27?";
+		String query = "What is the count of film actor actor_id [inverted] of actor #27?";
 		//String query = "What is the count of payment rental id ?";
-		String query = "What is the rental last update of rental #1?";
+		//String query = "What is the rental last update of rental #1?";
 
+
+        //mydb_simple
+		//String query = "What is the name of customer #1?";//inserendo un nuovo cliente per quel film il count aumenta
+        //------------------------------------------
 
 		//String query = "What are the award of Alain Connes?";
 		
